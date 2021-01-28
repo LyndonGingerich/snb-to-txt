de1e57ec77cb32e5293c1d29508b467882264ff4
Lyndon Gingerich <gundericusthemighty@gmail.com>
2021-01-28 08:28:15 -0600

    At least it closes now.

--- snbToTxt.py
+++ snbToTxt.py
@@ -1,1 +1,16 @@
+from os import path
+import re
+import zipfile
 
+def convertToTxt(snbFile):
+    zipfileObject = zipfile.ZipFile(snbFile)
+    zipfileObject.extract('snote/snote.xml')
+    zipfileObject.close()
+    with open('snote.xml', 'r') as xmlFile:
+        xmlText = xmlFile.read()
+    txtText = re.sub('<.*?>', '', xmlText)
+    txtFileName = snbFile.replace('.snb', '.txt')
+    with open(txtFileName, 'w') as txtFile:
+        txtFile.write(txtText)
+
+convertToTxt('Memo_20160202_122147724.snb') # for testing
